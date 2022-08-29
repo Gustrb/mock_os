4e2dff930e4c28bb70e7c302092ecb7f39323fce
Gustrb <gustavo@minhavisita.com.br>
2022-08-29 20:36:17 -0300

    feat(Kernel): Adding basic kernel with vga

---
+++ run.sh
@@ -0,0 +1,29 @@
+#assemble boot.s file
+as --32 src/boot/boot.s -o boot.o
+
+#compile kernel.c file
+gcc -m32 -c src/kernel/kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+
+#linking the kernel with kernel.o and boot.o files
+ld -m elf_i386 -T src/boot/linker.ld kernel.o boot.o -o mock_os.bin -nostdlib
+
+#check mock_os.bin file is x86 multiboot file or not
+grub-file --is-x86-multiboot mock_os.bin
+
+#building the iso file
+mkdir -p isodir/boot/grub
+
+cp mock_os.bin isodir/boot/mock_os.bin
+cp src/boot/grubfile.cfg isodir/boot/grub/grub.cfg
+
+grub-mkrescue -o mock_os.iso isodir
+
+#run it in qemu
+qemu-system-x86_64 -cdrom mock_os.iso
+
+# Cleanup
+rm -rf isodir
+rm boot.o
+rm kernel.o
+rm mock_os.bin
+rm mock_os.iso
---
+++ src/kernel/kernel.h
@@ -0,0 +1,34 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+#define VGA_ADDRESS 0xB8000
+#define BUFF_SIZE   2200
+
+uint16* vga_buffer;
+
+#define NULL 0
+
+typedef enum vga_color {
+  BLACK,
+  BLUE,
+  GREEN,
+  CYAN,
+  RED,
+  MAGENTA,
+  BROWN,
+  GREY,
+  DARK_GREY,
+  BRIGHT_BLUE,
+  BRIGHT_GREEN,
+  BRIGHT_CYAN,
+  BRIGHT_RED,
+  BRIGHT_MAGENTA,
+  YELLOW,
+  WHITE,
+} vga_color_t;
+
+#endif
