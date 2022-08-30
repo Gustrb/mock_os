693a0a507b10d1da00d15fe20097b16ef463d5e9
Gustrb <gustavo@minhavisita.com.br>
2022-08-30 20:30:31 -0300

    feat(Kernel/Vga): Documenting code

--- src/kernel/io/includes/vga.h
+++ src/kernel/io/includes/vga.h
@@ -8,6 +8,7 @@ typedef unsigned int uint32;
 #define VGA_ADDRESS 0xB8000
 #define BUFF_SIZE 2200
 #define NULL 0
+#define PRIVATE static
 
 uint16 *vga_buffer;
 
@@ -30,12 +31,55 @@ typedef enum vga_color {
   WHITE,
 } vga_color_t;
 
-void print_int(int num);
-void print_string(char *str);
-void print_char(char ch);
-void print_new_line();
+/**
+ * Initializes the vga buffer with the given fore_color and back_color, and clears the vga_buffer.
+ *
+ * @param fore_color the foreground color is used to paint the character.
+ * @param back_color the background color is used to paint the screen.
+ */
 void init_vga(uint8 fore_color, uint8 back_color);
+
+/**
+ * Prints an integer to the vga buffer
+ *
+ * @param num the integer to be printed
+ */
+void print_int(int num);
+
+/**
+ * prints a null terminated string to the vga buffer
+ *
+ * This method assumes the string is null terminated, and its memory is valid and allocated
+ * if its null or it points to invalid memory it will cause a segmentation fault.
+ *
+ * @param str the string to be printed
+ */
+void print_string(char *str);
+
+/**
+ * Prints the char to the vga buffer using it ascii code(vga_entry)
+ *
+ * @param ch the character to be printed
+ */
+void print_char(char ch);
+
+/**
+ * Prints a new line in the vga buffer
+ *
+ * If the next line is going to overflow the buffer then it clear the buffer and reset the index to 0.
+ */
+void print_new_line();
+
+/**
+ * Clears the vga buffer with the given fore_color and back_color, and sets each vga uint16 to 0.
+ *
+ * This method assumes the vga buffer is allocated and can be used so
+ * if its null or it points to invalid memory it will cause a segmentation fault.
+ *
+ * @param buffer is the vga buffer to be cleared.
+ * @param fore_color the foreground color is used to paint the character.
+ * @param back_color the background color is used to paint the screen.
+ */
 void clear_vga_buffer(uint16 **buffer, uint8 fore_color, uint8 back_color);
-uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color);
 
 #endif
--- src/kernel/io/vga.c
+++ src/kernel/io/vga.c
@@ -1,16 +1,29 @@
 #include "includes/vga.h"
 #include "../../lib/string/string.h"
 
-//index for video buffer array
+// Currently the vga buffer is located at 0xB8000
 uint32 vga_index;
-//counter to store new lines
+
+// Counter to store the number of lines that have been printed
 static uint32 next_line_index = 1;
-//fore & back color values
-uint8 g_fore_color = WHITE, g_back_color = BLUE;
-//digit ascii code for printing integers
+
+// Global foreground and background color values
+uint8 g_fore_color = WHITE;
+uint8 g_back_color = BLUE;
+
+// The ascii codes for all the numeric digits
 int digit_ascii_codes[10] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
 
-uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color) {
+/**
+ * Gets the vga uint16 that corresponds to that fore_ground color and back_ground color
+ * and the character to be printed.
+ *
+ * @param ch the character to be printed.
+ * @param fore_color the foreground color is used to paint the character.
+ * @param back_color the background color is used to paint the screen.
+ * @return uint16 the vga uint16 that corresponds to the fore_ground color, back_ground color and the character to be printed.
+ */
+PRIVATE uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color) {
   uint16 ax = 0;
   uint8 ah = 0, al = 0;
 
@@ -25,56 +38,65 @@ uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color) {
   return ax;
 }
 
-//clear video buffer array
 void clear_vga_buffer(uint16 **buffer, uint8 fore_color, uint8 back_color) {
-  uint32 i;
-  for(i = 0; i < BUFF_SIZE; i++){
+  // Go through the buffer and set each uint16 to 0.
+  for(uint32 i = 0; i < BUFF_SIZE; i++){
     (*buffer)[i] = vga_entry(NULL, fore_color, back_color);
   }
+
+  // Reset the index to 0, and the next_line_index to 1.
   next_line_index = 1;
   vga_index = 0;
 }
 
-//initialize vga buffer
 void init_vga(uint8 fore_color, uint8 back_color) {
-  vga_buffer = (uint16*)VGA_ADDRESS;
+  // Cast the vga_buffer to a uint16 pointer, and set it to the VGA_ADDRESS.
+  // because it is the address of all vgas buffer, 0xB8000
+  vga_buffer = (uint16*) VGA_ADDRESS;
+
+  // Clears the buffer itself so everything is blank.
   clear_vga_buffer(&vga_buffer, fore_color, back_color);
+
+  // Set the fore_color and back_color.
   g_fore_color = fore_color;
   g_back_color = back_color;
 }
 
-/*
-increase vga_index by width of row(80)
-*/
 void print_new_line() {
+  // If it is going to overflow reset it and clear the buffer.
   if (next_line_index >= 55) {
     next_line_index = 0;
     clear_vga_buffer(&vga_buffer, g_fore_color, g_back_color);
   }
-  vga_index = 80*next_line_index;
+
+  // Increment the next_line_index.
+  vga_index = 80 * next_line_index;
   next_line_index++;
 }
 
-//assign ascii character to video buffer
 void print_char(char ch) {
+  // Inserts the vga uint16 into the vga buffer.
   vga_buffer[vga_index] = vga_entry(ch, g_fore_color, g_back_color);
+
+  // Incremets the index
   vga_index++;
 }
 
-//print string by calling print_char
 void print_string(char *str) {
   uint32 index = 0;
 
+  // Foreach char print it to the buffer;
   while (str[index]){
     print_char(str[index]);
     index++;
   }
 }
 
-//print int by converting it into string
-//& then printing string
 void print_int(int num) {
+  // Stack allocate a buffer to store the number, containing enough space for the number and a null terminator.
   char str_num[digit_count(num) + 1];
+  // Insert the number into the buffer.
   itoa(num, str_num);
+  // Print the buffer.
   print_string(str_num);
 }
